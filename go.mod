e4d961a2fdbbc0df8900235a46592cec22553a13
alj17 <hickgrum@gmail.com>
2022-06-11 11:50:08 -0700

    Add license id

--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require (
 	github.com/josharian/native v1.0.0 // indirect
 	github.com/mdlayher/netlink v1.6.0 // indirect
 	github.com/mdlayher/socket v0.1.1 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
+	golang.org/x/tools v0.1.11 // indirect
 )
--- pkg/inet/ip.go
+++ pkg/inet/ip.go
@@ -1,20 +1,21 @@
+/* SPDX-License-Identifier: MIT */
 package inet
 
 import "net"
 
 type IP struct {
-	addr	uint32
+	addr uint32
 }
 
 func IPv4(a, b, c, d uint8) IP {
 	var ip uint32
 
-	ip =	(uint32(a) << 24) |
+	ip = (uint32(a) << 24) |
 		(uint32(b) << 16) |
-		(uint32(c) << 8)  |
+		(uint32(c) << 8) |
 		(uint32(d))
 
-	return IP{ addr: ip }
+	return IP{addr: ip}
 }
 
 func (ip IP) NetIP() net.IP {
@@ -31,3 +32,14 @@ func (ip IP) NetIP() net.IP {
 func (ip IP) String() string {
 	return ip.NetIP().String()
 }
+
+type Protocol uint8
+
+const (
+	ICMP Protocol = 1
+	IPIP = 4,
+	TCP = 6,
+	UDP = 17,
+	ESP = 50,
+	AH = 51
+)
